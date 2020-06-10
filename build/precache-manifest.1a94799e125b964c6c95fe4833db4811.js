d6bc3504078d94ef20e0693a565492dc1acaa748
Origho-precious <orighi8@gmail.com>
2020-06-10 02:57:16 +0100

    add build

---
+++ build/precache-manifest.1a94799e125b964c6c95fe4833db4811.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b5f24e2bd390ce11ff3a478494867e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c22e6a67bab44e1a711b",
+    "url": "/static/css/main.d3c1b974.chunk.css"
+  },
+  {
+    "revision": "9d34a1f49a768ab6411d",
+    "url": "/static/js/2.d01342c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d01342c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c22e6a67bab44e1a711b",
+    "url": "/static/js/main.fecc6513.chunk.js"
+  },
+  {
+    "revision": "b2c1f8c1712f66222f13",
+    "url": "/static/js/runtime-main.d4548fc6.js"
+  }
+]);
